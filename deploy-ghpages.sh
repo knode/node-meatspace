8f789edeb0d07a5a73b4cd57d8c467e6f9ad71e6
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-01-17 00:05:33 +0100

    travis fix

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,12 +3,12 @@ node build.js
 (
   mkdir pages
   cd pages
-  git clone --branch=gh-pages https://${GH_TOKEN}@${GH_REF}
+  git clone --branch=gh-pages "https://${GH_TOKEN}@${GH_REF}"
   git checkout gh-pages
   cp ../index.html /index.html
-  git config user.name 'Travis-CI'
-  git config user.email 'dev@nodemeatspace.com'
+  git config user.name "Travis-CI"
+  git config user.email "dev@nodemeatspace.com"
   git add index.html
-  git commit -m 'Deployed to Github Pages'
+  git commit -m "Deployed to Github Pages"
   git push --force --quiet origin gh-pages
 )
