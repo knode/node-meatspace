3c5934973845a3b5b3e959fdbab20660955c29b3
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-01-17 07:52:36 +0100

    countryiso fix

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -7,6 +7,7 @@ node build.js
   git config user.name "Travis-CI"
   git config user.email "travis@nodemeatspace.com"
   cp ../CNAME ./CNAME
+  cp ../countryiso.js ./countryiso.js
   git add .
   git commit -m "Deployed to Github Pages"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages >/dev/null
